73ad5e6734c304861e626606b6107454941a1110
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-01-09 17:05:53 +0100

    feat: neues user Schema und die passwörter werden vor dem speichern gehashd

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+//das kommt immer dazwischen wenn ein neuer user gesaved wird
+userSchema.pre('save', async function (next) {
+    console.log("Vorher:", this.password);
+    this.password = await bcrypt.hash(this.password, 10); // hashen
+
+    console.log("Nachher:", this.password);
+    next();
+});
+const USER = mongoose.model('User', userSchema);
+module.exports = USER;
+
