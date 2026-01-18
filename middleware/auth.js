263dfd8462729ea3a21de283b020a65e03d3c43f
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-01-18 15:51:24 +0100

    feat: wenn token abgelaufen ist dann bekommt man nun ein error accestoken abgelaufen zurück damit man weiß das man nun refreshen muss

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1]; //man muss spittten da Bearer drin ist und den braucht man nicht
-    if (!token) {
-        return res.status(401).json({ error: "Kein Token" });
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ error: "kein AccesToken" });
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ error: "Token ungültig" });
-        } else {
-            req.userId = decoded.id;  // Jetzt hat der Controller Zugriff!
-            next();
+    const token = authHeader.split(' ')[1]; //man muss spittten da Bearer drin ist und den braucht man nicht
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;  // Jetzt hat der Controller Zugriff!
+        next();
+    } catch (err) {
+        if (err.name === "TokenExpiredError") {
+            return res.status(401).json({ error: "Acces-Token-Abgelaufen" });
         }
-    });
+        return res.status(401).json({ error: "Token ungültig" });
+    }
 };
 
 module.exports = authMiddleware;
